a9ae3c03193ec31d1989d9b5087a638eea4d7341
xiaoketeng <1531464644@qq.com>
2020-06-21 21:40:27 +0800

    '完成主题自定义和按需引入antd'

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+/*
+后台管理路由组件
+ */
+export default class Admin extends Component {
+    constructor (props) {
+        super(props);
+    }
+    
+    render () {
+        return (
+            <div>
+                Admin
+            </div>
+        )
+    }
+    
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+// 登陆的路由组件
+export default class Login extends Component {
+    constructor (props) {
+        super(props);
+    }
+    
+    render () {
+        return (
+            <div>
+                Login
+            </div>
+        )
+    }
+    
+}
